db0218474fae839bfc8b332f346aa12fb4ae1932
natalie-eizips <104609729+natalie-eizips@users.noreply.github.com>
2024-12-01 21:48:58 +0200

    update

--- MyString.java
+++ MyString.java
@@ -18,13 +18,42 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String lower = "";
+        for (int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if (str.charAt(i) >= 65 && str.charAt(i) <= 90){
+                ch = (char)(str.charAt(i) + 32);
+            }
+            lower += ch;
+        }
+        return lower;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        int n1 = str1.length();
+        int n2 = str2.length();
+        String str = "";
+
+        if (n2 == 0){
+            return true;
+        }
+
+        if (n1 < n2){
+            return false;
+        }
+
+        for (int i = 0; i <= n1 - n2; i++){
+            int j;
+            for (j = 0; j < n2; j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    break; 
+                }
+            }
+            if (j == n2) {
+                return true; 
+            }
+        }
+        return false; 
     }
 }
